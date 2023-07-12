88856988de4bd684ddb87fe6497f5d9255216d58
auladeproyectos <auladeproyectos@gmail.com>
2023-07-11 20:59:11 -0500

    siguiente commit

--- main.go
+++ main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/Auladeproyectos/godesde0/variables"
+	"runtime"
 )
 
 func main() {
 
-	estado, texto := variables.ConviertoaTexto(1677)
+	/*estado, texto := variables.ConviertoaTexto(1677)
 	fmt.Println(estado)
-	fmt.Println(texto)
+	fmt.Println(texto) */
 
+	os := runtime.GOOS
+
+	if os == "Linux." || os == "OS X." {
+		fmt.Println("Esto no es windows")
+	} else {
+		fmt.Println("Esto es windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "Linux":
+		fmt.Println("Esto es Linux")
+
+	case "Darwin":
+		fmt.Println("Esto es darwin")
+
+	default:
+		fmt.Printf("%s \n", os)
+
+	}
 }
